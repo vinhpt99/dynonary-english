e8806bb57e2073bc584a7f2fcb2ebe47812422c2
vinhpt99 <vinhpt72@gmail.com>
2024-12-19 22:42:32 +0700

    add tailwind

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import vuetify from "./plugins/vuetify"
-
+import './main.css'
 import App from './App.vue'
 import router from './router'
 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <p class="font-bold">This text is bold.</p>
+  </div>
 </template>
+<script setup lang="ts">
+
+</script>
